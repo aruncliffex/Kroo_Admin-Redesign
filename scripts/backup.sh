4791cb309453768ef77037c597e9af802b24ee6c
arun.tewatia <arun.tewatia@cliffex.com>
2017-02-23 14:01:10 +0530

    Updated backup filename format

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tar -czvf /home/ubuntu/KrooAdminBackup/backup-$(date +%Y-%m-%d).tar.gz /var/www/html/Kroo_Admin-Redesign_Demo/
+tar -czvf /home/ubuntu/KrooAdminBackup/backup-$(date +%Y-%m-%d_%H:%M:%S).tar.gz /var/www/html/Kroo_Admin-Redesign_Demo/
 rm -r /var/www/html/Kroo_Admin-Redesign_Demo/*
 
  
