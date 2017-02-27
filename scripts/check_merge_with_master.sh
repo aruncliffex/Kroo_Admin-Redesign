de5b1fd8d0cd1ffc0224bf6137ce3f6a1741b5cf
arun.tewatia <arun.tewatia@cliffex.com>
2017-02-27 17:01:20 +0530

    Removed patched

--- scripts/check_merge_with_master.sh
+++ scripts/check_merge_with_master.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 git checkout development
-git format-patch master --stdout > patches/dev-$(date +%Y-%m-%d_%H:%M:%S).patch
-var=`git apply --check crazy.patch 2>&1`
+git format-patch master --stdout > temp.patch
+var=`git apply --check temp.patch 2>&1`
 var1=`echo $var| cut -d' ' -f1`
 
-if [ "$var1" == "error:" ]
+if [ "$var1" != "error:" ]
 then
-	printf "Error found: Merge with master having conflicts\n\n"
-	echo `git apply --check crazy.patch`
-	exit 50
-else
 	echo "Success!"
+	rm temp.patch
 	exit 0
+else
+	printf "Error found: Merge with master having conflicts\n\n"
+	echo `git apply --check temp.patch`
+	rm temp.patch
+	exit 50
 fi
