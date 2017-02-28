bf4437231472bae14a471b87fb80a7ffc31005ab
arun.tewatia <arun.tewatia@cliffex.com>
2017-02-28 11:11:33 +0530

    Edited check merge script

--- scripts/check_merge_with_master.sh
+++ scripts/check_merge_with_master.sh
@@ -5,15 +5,13 @@ var=`git merge --no-commit development  2>&1`
 var1=`echo $var| cut -d' ' -f1-4`
 var2=`echo $var| cut -d' ' -f1-2`
 
-echo $var1
-echo $var2
 if [ "$var1" == "Automatic merge went well;" || "$var2" == "Already up-to-date."]
 then
 	echo "Success!"
 	exit 0
 else
 	printf "Error found: Merge with master having conflicts\n\n"
-	printf "$var
+	printf "$var"
 	git merge --abort
 	exit 50
 fi
