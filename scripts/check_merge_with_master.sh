fcec798666a3aad5bd62f844eb49dda33c7e314c
arun.tewatia <arun.tewatia@cliffex.com>
2017-02-28 11:42:19 +0530

    Updated check merge script

--- scripts/check_merge_with_master.sh
+++ scripts/check_merge_with_master.sh
@@ -4,8 +4,10 @@ git checkout master
 var=`git merge --no-commit development  2>&1`
 var1=`echo $var| cut -d' ' -f1-4`
 var2=`echo $var| cut -d' ' -f1-2`
-
-if [[ "$var1" == "Automatic merge went well;" ]] || [[ "$var2" == "Already up-to-date." ]]
+var3=`echo $var| cut -d' ' -f1-8`
+if [[ "$var1" == "Automatic merge went well;" ]] || 
+   [[ "$var2" == "Already up-to-date." ]] || 
+   [[ "$var3" == "merge: development - not something we can merge" ]]
 then
         echo "Success!"
         exit 0
