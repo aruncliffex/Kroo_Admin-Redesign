87009ed347f9971e0325ca65cb0709ce101abfd2
arun.tewatia <arun.tewatia@cliffex.com>
2017-02-28 10:20:09 +0530

    Added final merge script to appspec.yml

---
+++ scripts/merge_with_master.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git checkout master
+var=`git merge development`
+error_code=$?
+echo $error_code
+if [ "$error_code" == 0 ]
+then
+	echo "Merged with master branch successfull !"
+	exit 0
+else
+	printf "Error found: Merge with master unsuccessfull !  \n\n"
+	printf "$var \n\n"
+	git merge --abort
+	exit 50
+fi
