d42cfad3f3b922d36f35e39249b52621a75f2c29
arun.tewatia <arun.tewatia@cliffex.com>
2017-03-01 10:31:27 +0530

    Updated git push script

--- scripts/merge_with_master.sh
+++ scripts/merge_with_master.sh
@@ -9,6 +9,10 @@ echo $error_code
 if [ "$error_code" == 0 ]
 then
 	echo "Merged with master branch successfull !"
+	git remote set-url origin  git+ssh://git@github.com/aruncliffex/Kroo_Admin-Redesign.git
+	git add .
+	git commit -m"Commited merge from Master+Development branch"
+	git push origin master
 	exit 0
 else
 	printf "Error found: Merge with master unsuccessfull !  \n\n"
